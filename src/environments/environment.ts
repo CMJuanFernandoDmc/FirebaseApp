b394ac4aa49a7d17d239140dc197580db5e69a41
CMJuanFernandoDmc <151117518+CMJuanFernandoDmc@users.noreply.github.com>
2024-02-07 19:37:32 -0500

    EjerciciosFirebase
    Implementar 4 botones para encender y apagar LEDs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA9cftwrpLV0shPhc6LG2bfTlXFlgniSFI",
+    authDomain: "ngbytes-fireauth-7486c.firebaseapp.com",
+    projectId: "ngbytes-fireauth-7486c",
+    storageBucket: "ngbytes-fireauth-7486c.appspot.com",
+    messagingSenderId: "487994065015",
+    appId: "1:487994065015:web:c901daf545202536f48672"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
